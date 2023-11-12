cd2bc718c46a1608004449e64aa8f184d1b8ca93
Agustin <87285884+AgustinBertero@users.noreply.github.com>
2023-11-12 20:58:02 +0100

    Creo consulta y tabla de productos con mayor Revenue

--- rentabilidad.py
+++ rentabilidad.py
@@ -3,3 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 conn = sqlite3.connect("Nortwind.db")
+
+query = '''
+    SELECT ProductName, SUM(Price * Quantity) as Revenue
+    FROM OrderDetails od
+    JOIN Products p ON p.ProductID = od.ProductID
+    GROUP BY od.ProductID
+    ORDER BY Revenue DESC
+    LIMIT 10
+    '''
+    
+top_products = pd.read_sql_query(query,conn)
+
+top_products.plot(x="ProductName",y="Revenue",kind="bar",figsize=(10,5),legend=False)
